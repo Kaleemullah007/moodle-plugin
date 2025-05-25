25e89d3b491dc2c2fc8a046c9dbaf8f4dba55257
kaleemullah <kaleemullahdev@gmail.com>
2025-05-25 21:53:55 +0500

    Add Es langs to

---
+++ lang/es/local_registration_validation.php
@@ -0,0 +1,34 @@
+<?php
+// Language strings for registration plugin
+
+$string['pluginname'] = 'Student Registration';
+$string['dpicode'] = 'DPI/CUI (13-digit)';
+$string['dpicodeerror'] = 'DPI must be exactly 13 digits.';
+$string['usernameexists'] = 'Username already exists.';
+$string['emailexists'] = 'Email already registered.';
+$string['dpiexists'] = 'DPI already registered.';
+$string['username'] = 'Username';
+$string['usernameinvalid'] = 'Username can only contain letters, numbers, underscores or hyphens.';
+$string['password'] = 'Password';
+$string['passwordminlength'] = 'Password must be at least 6 characters.';
+$string['email'] = 'Email';
+$string['invalidemail'] = 'Invalid email address.';
+$string['firstname'] = 'First Name';
+$string['lastname'] = 'Last Name';
+$string['gender'] = 'Gender';
+$string['male'] = 'Male';
+$string['female'] = 'Female';
+$string['other'] = 'Other';
+$string['phone'] = 'Phone';
+$string['phoneinvalid'] = 'Invalid phone number format.';
+$string['age'] = 'Age';
+$string['invalidage'] = 'Age must be a number between 18 and 120.';
+$string['department'] = 'Department';
+$string['municipality'] = 'Municipality';
+$string['ethnicity'] = 'Ethnicity';
+$string['professionalsector'] = 'Professional Sector';
+$string['institution'] = 'Institution';
+$string['professionalcollege'] = 'Professional College';
+$string['membernumber'] = 'Member Number';
+$string['requiredfield'] = 'This field is required.';
+$string['register'] = 'Register';
